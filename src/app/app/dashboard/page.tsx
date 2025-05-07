01bb70a4be7c95d7d5a8481e6d28b1da25ef5405
alexpolato <alexandrecpolato@gmail.com>
2025-05-06 21:07:58 -0300

    feat: set backend deploy url

--- src/app/app/dashboard/page.tsx
+++ src/app/app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import { BarChartComponent } from "@/components/bar-chart";
 import { CircleChart } from "@/components/circle-chart";
+import DashboardComp from "@/components/dashboard-comp/dashboard";
 import { DataTableDemo } from "@/components/data-table";
 import { RadialChartComponent } from "@/components/radial-chart";
 
@@ -10,8 +11,11 @@ export default function Dashboard() {
         <h1 className="text-3xl font-semibold">Dashboard</h1>
         <div className="">
           <h1 className="text-xl font-semibold mt-5">Gráficos</h1>
+          <div>
+            <DashboardComp />
+          </div>
 
-          <div className="grid grid-cols-3 w-full  gap-4">
+          {/* <div className="grid grid-cols-3 w-full  gap-4">
             <div className="bg-red-50 ">
               <CircleChart />
             </div>
@@ -21,11 +25,7 @@ export default function Dashboard() {
             <div className="bg-red-50 ">
               <BarChartComponent />
             </div>
-          </div>
-          <div className="mt-10">
-            <h1 className="font-semibold text-xl">Tabela de Prioridade</h1>
-            <DataTableDemo />
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
---
+++ src/components/dashboard-comp/dashboard.tsx
@@ -0,0 +1,52 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import fetchData from "@/components/dashboard-comp/fetch-data";
+import { DairyData, DataTableDash } from "./data-table";
+
+export default function Dashboard() {
+  const [data, setData] = useState<any>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    // Fetch data when the component mounts
+    fetchDataFromAPI();
+  }, []);
+
+  const fetchDataFromAPI = async () => {
+    try {
+      const result = await fetchData();
+      setData(result); // Store the fetched data in state
+    } catch (err) {
+      console.error("Error fetching data:", err);
+      setError(
+        err instanceof Error ? err.message : "An unknown error occurred."
+      );
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div style={{ color: "red" }}>Error: {error}</div>;
+  }
+  // Adicione esta verificação antes de renderizar a tabela
+  if (!data || !data.data || !Array.isArray(data.data)) {
+    return <div>No data available or data is in wrong format</div>;
+  }
+  return (
+    <div>
+      <h1 className="mt-10 font-medium text-xl">Dashboard Data</h1>
+      <div>
+        <DataTableDash data={data.data as DairyData[]} />
+      </div>
+      {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+      {/* Pass the fetched
+      {/* Display the fetched data */}
+    </div>
+  );
+}
